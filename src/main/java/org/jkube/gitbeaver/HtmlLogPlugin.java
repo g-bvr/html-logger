5a5d7c8dfb77d2f6da216520983fe497eb58ae50
Jan Kneissler <jan.kneissler@breuninger.de>
2024-09-02 15:17:09 +0200

    removed dependency to java base, added files from java base, removed parent pom, added various plugins and properties from previous parent pom.

--- src/main/java/org/jkube/gitbeaver/HtmlLogPlugin.java
+++ src/main/java/org/jkube/gitbeaver/HtmlLogPlugin.java
@@ -3,7 +3,7 @@
     import org.jkube.gitbeaver.htmllog.HtmlLogger;
     import org.jkube.gitbeaver.htmllog.WriteHtmlLogsCommand;
     import org.jkube.gitbeaver.plugin.SimplePlugin;
-    import org.jkube.logging.Log;
+    import org.jkube.gitbeaver.logging.Log;
 
 public class HtmlLogPlugin extends SimplePlugin {
 
